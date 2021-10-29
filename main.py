d0bd623bce2edae962ef5abba6af81726bd16578
Leho <leho@leho.dev>
2021-10-29 14:19:57 +1100

    init

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord import member
+from discord.ext import commands
+
+TOKEN = "PUT YOUR TOKEN HERE"
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(intents=intents, command_prefix='>')
+
+@bot.event
+async def on_ready():
+    print("Logged in as: " + bot.user.name + "#" + bot.user.discriminator)
+    print("Ready")
+
+@bot.command()
+async def id(ctx):
+    for member in ctx.guild.members:
+        for role in member.roles:
+            if role.name != "Verified":
+                print(f"Banning {str(member.id) + str(member)}")
+                try:
+                    await member.ban()
+                except Exception as e:
+                    print("Error Banning")
+
+bot.run(str(TOKEN))
